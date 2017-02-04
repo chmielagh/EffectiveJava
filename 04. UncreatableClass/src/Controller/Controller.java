adb6e48a0c51c2856aa65d938af550718ce98609
Konrad Chmiel <konrad.chmiel@qumak.pl>
2017-02-04 14:59:26 +0100

    section 4. with example

---
+++ 04. UncreatableClass/src/Controller/Controller.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import Model.Util;
+
+/**
+ * Created by kchmiel on 04.02.2017.
+ */
+public class Controller {
+
+   public static void main(String[] args) {
+
+      System.out.println("Some util field: " + Util.SOME_STRING);
+      // Util util = new Util(); - no nie mozna
+   }
+}
---
+++ 04. UncreatableClass/src/Model/Util.java
@@ -0,0 +1,14 @@
+package Model;
+
+/**
+ * Created by kchmiel on 04.02.2017.
+ */
+public class Util {
+   public static final String SOME_STRING = "Some String";
+   public static final String SOME_STRING2 = "Some String2";
+
+   private Util() {
+      // to disable createing instance of class
+      throw new AssertionError();
+   }
+}
